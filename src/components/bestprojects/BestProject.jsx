cc89c8f49ad8046bf1b0fbd56d053834580f52c2
Uniqcle <uniqcle@yandex.ru>
2024-02-24 20:01:53 +0700

    best projects

--- src/components/bestprojects/BestProject.jsx
+++ src/components/bestprojects/BestProject.jsx
@@ -1,80 +1,51 @@
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { ProjectContext } from "../../App";
+
 const BestProjects = () => {
+  const [best, setBest] = useState([]);
+  const projects = useContext(ProjectContext);
+
+  const filterByBest = () => {
+    const bestProjects = projects.filter((p) => p.best === true);
+
+    setBest(bestProjects);
+  };
+
+  const renderBest = (bestProjects) => {
+    const bestItems = bestProjects.map((p) => {
+      console.log(p);
+      const { id, title, img } = p;
+      return (
+        <li key={id} className="project">
+          <Link to={`projects/${id}`}>
+            <img src={img} className="project__img" alt="project img" />
+            <h3 className="project__title">{title}</h3>
+          </Link>
+        </li>
+      );
+    });
+
+    return <ul className="projects">{bestItems}</ul>;
+  };
+
+  useEffect(() => {
+    filterByBest();
+  }, []);
+
+  const items = renderBest(best);
+
   return (
     <main className="section">
       <div className="container">
         <h1 className="title-h1">Best Projects</h1>
-        <ul className="projects">
-          <li className="project">
-            <a href="./project.html">
-              <img
-                src="./imgs/projects/01.jpg"
-                className="project__img"
-                alt="project img"
-              />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li>
 
-          <li className="project">
-            <a href="#">
-              <img
-                src="./imgs/projects/01.jpg"
-                className="project__img"
-                alt="project img"
-              />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li>
-
-          <li className="project">
-            <a href="#">
-              <img
-                src="./imgs/projects/01.jpg"
-                className="project__img"
-                alt="project img"
-              />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li>
-
-          <li className="project">
-            <a href="#">
-              <img
-                src="./imgs/projects/01.jpg"
-                className="project__img"
-                alt="project img"
-              />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li>
-
-          <li className="project">
-            <a href="#">
-              <img
-                src="./imgs/projects/01.jpg"
-                className="project__img"
-                alt="project img"
-              />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li>
-
-          <li className="project">
-            <a href="#">
-              <img
-                src="./imgs/projects/01.jpg"
-                className="project__img"
-                alt="project img"
-              />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li>
-        </ul>
+        {items}
 
         <div>
-          <a href="./projects.html" className="btn-all">
+          <Link to="projects" className="btn-all">
             See all projects
-          </a>
+          </Link>
         </div>
       </div>
     </main>
